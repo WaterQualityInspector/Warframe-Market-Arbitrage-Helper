a16000e82fa8f330317f013f6d50d83e8ec184e1
CantFindBacon <114840936+CantFindBacon@users.noreply.github.com>
2023-08-12 14:09:39 -0400

    conversion to asynchronous calls

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import scraper
+import asyncio
+import aiohttp
 
 pd.set_option('display.max_columns', None)
 pd.set_option('display.max_colwidth', 30)  # Set maximum column width
@@ -8,7 +10,7 @@ pd.set_option('display.expand_frame_repr', False)  # Do not wrap to multiple lin
 wfm_url = "https://api.warframe.market/v1/items"
 
 #print(scraper.GetItemMarketStatistics("ammo_drum"))
-print(scraper.GetAllSummarizedMarketStatistics())
+print(asyncio.run(scraper.GetAllSummarizedMarketStatistics()))
 
 
 #items_df = scraper.GetAllItemsList(wfm_url)
--- scraper.py
+++ scraper.py
@@ -1,33 +1,39 @@
 from datetime import datetime, timezone
-
 from tqdm import tqdm
 
+import asyncio
 import numpy as np
-import time
 import pandas as pd
 import requests
-import re
+
+jwt_key = "AZ"
+key_header = {
+    'Authorization': f"JWT {jwt_key}"
+}
 
 def_wfm_url = "https://api.warframe.market/v1/items"  # default access link
-rq_min_delay = 0.05
+rq_min_delay = 1
 accounted_days = 30
-item_exclusion_tags=['barrel','receiver','stock','chassis','neuroptics','systems','relic', 'emote','pouch', 'hilt','guard',
-                     'string','lower_limb','upper_limb','key','handle','ornament','capsule',
-                     'casing','weapon_','engine','handle','blade','grip','scene','carapace','cerebrum','head',
-                     'motor','blade','barrels','link','receivers','prime_blueprint','chassis_blueprint', 'neuroptics_blueprint',
-                     'systems_blueprint', 'prime_harness','prime_wings','prime_chain', 'vandal_blueprint', 'wraith_blueprint',
-                     'prime_disc','left_gauntlet','right_gauntlet']
+item_exclusion_tags = ['barrel', 'receiver', 'stock', 'chassis', 'neuroptics', 'systems', 'relic', 'emote', 'pouch',
+                       'hilt', 'guard',
+                       'string', 'lower_limb', 'upper_limb', 'key', 'handle', 'ornament', 'capsule',
+                       'casing', 'weapon_', 'engine', 'handle', 'blade', 'grip', 'scene', 'carapace', 'cerebrum',
+                       'head',
+                       'motor', 'blade', 'barrels', 'link', 'receivers', 'prime_blueprint', 'chassis_blueprint',
+                       'neuroptics_blueprint',
+                       'systems_blueprint', 'prime_harness', 'prime_wings', 'prime_chain', 'vandal_blueprint',
+                       'wraith_blueprint',
+                       'prime_disc', 'left_gauntlet', 'right_gauntlet']
 
 
 # 0 for stuff without mod ranks (e.g. frame chassis)
 # Whole row being 0 if the trade history is not available (no trades are available. e.g. adept_surge)
 
-
 def GetAllItemsList(wfm_url=def_wfm_url):  # Gets the list of ALL items that are available on the WFM
-    r = requests.get(wfm_url, timeout=2)
-    #print(f"Items List HTML Response: {r.status_code}\n")
+    r = requests.get(wfm_url, timeout=5)
+    # print(f"Items List HTML Response: {r.status_code}\n")
 
-    exclusion_pattern = r'(?:'+'|'.join(item_exclusion_tags) + r')\b'
+    exclusion_pattern = r'(?:' + '|'.join(item_exclusion_tags) + r')\b'
 
     items_list_json = r.json()['payload']['items']
     items_list_df = pd.DataFrame(items_list_json)
@@ -40,8 +46,8 @@ def GetAllItemsList(wfm_url=def_wfm_url):  # Gets the list of ALL items that are
 
 
 def GetItemInformation(item_url_name, wfm_url=def_wfm_url):  # Gets all the information of a prompted item
-    r = requests.get(f"{wfm_url}/{item_url_name}")
-    #print(f"Item Info HTML Response: {r.status_code}\n")
+    r = requests.get(f"{wfm_url}/{item_url_name}", headers=key_header)
+    # print(f"Item Info HTML Response: {r.status_code}\n")
 
     item_info_df = pd.DataFrame(r.json()['payload']['item']['items_in_set'])
     item_info_df.index = item_info_df.url_name
@@ -59,7 +65,7 @@ def GetItemOrderInformation(item_url_name, row_limit=8,
                             wfm_url=def_wfm_url):  # Gets all the order information of a prompted item
     r = requests.get(f"{wfm_url}/{item_url_name}/orders")
 
-    #print(f"Item Order HTML Response: {r.status_code}\n")
+    # print(f"Item Order HTML Response: {r.status_code}\n")
 
     item_orders_df = pd.DataFrame(r.json()['payload']['orders'])
 
@@ -103,7 +109,8 @@ def GetItemOrderInformation(item_url_name, row_limit=8,
     return item_orders_df, item_sells_df, item_buys_df, current_spread
 
 
-def CreateEmptyHistory(): # this is used in case the item has invalid data, stuff like no transaction within date range, and etc.
+def CreateEmptyHistory():  # this is used in case the item has invalid data, stuff like no transaction within date
+    # range, etc.
     # please match the backup_columns with whatever the columns should be if the item is valid.
     error_columns = (
         'datetime', 'volume', 'min_price', 'max_price', 'open_price', 'closed_price', 'avg_price', 'wa_price',
@@ -123,16 +130,18 @@ def CreateEmptyHistory(): # this is used in case the item has invalid data, stuf
 
 
 def CheckTradeHistory(item_url_name, wfm_url=def_wfm_url):
-    r = requests.get(f"{wfm_url}/{item_url_name}/statistics")
-    item_trade_hist_df = pd.DataFrame(r.json()['payload']['statistics_closed']['90days'])
-    #print(f"\nItem Market Statistics HTML Response: {r.status_code}\n")
+    # some items do not have a single trade record, so it will return an empty JSON.
+    # this is to process those and spit out something that can be commonly processed.
+    # CreateEmptyHistory() is the function that generates "empty" df.
 
-    if item_trade_hist_df.empty:
-        print(f"\n{item_url_name}'s market data is not found!")
-
-        return CreateEmptyHistory()
-    else:
+    r = requests.get(f"{wfm_url}/{item_url_name}/statistics", headers=key_header)
+    if r.status_code == 200:
+        print(f"\n{item_url_name} [{r.status_code}]")
+        item_trade_hist_df = pd.DataFrame(r.json()['payload']['statistics_closed']['90days'])
         return item_trade_hist_df
+    else:
+        print(f"\n{item_url_name} is unreachable. [{r.status_code}]")
+        return CreateEmptyHistory()
 
 
 def GetItemMarketStatistics(item_url_name, wfm_url=def_wfm_url,
@@ -140,19 +149,24 @@ def GetItemMarketStatistics(item_url_name, wfm_url=def_wfm_url,
 
     item_statistics_closed_df = CheckTradeHistory(item_url_name, wfm_url)
 
-    # sort and trim by days wanted, take out nulls
+    # clean and sort some columns since sometimes the returned json aren't quite consistent
+    if 'datetime' in item_statistics_closed_df.columns:
+        item_statistics_closed_df = item_statistics_closed_df.sort_values(['datetime'], ascending=False).reset_index(
+            drop=True)
+    else:
+        item_statistics_closed_df['datetime'] = datetime(1900, 1, 1, 0, 0)
+        item_statistics_closed_df['datetime'] = pd.to_datetime(item_statistics_closed_df['datetime'], utc=True)
 
-    item_statistics_closed_df = item_statistics_closed_df.sort_values(['datetime'], ascending=False).reset_index(
-        drop=True)
+    if 'id' in item_statistics_closed_df.columns:
+        item_statistics_closed_df = item_statistics_closed_df.drop(['id'], axis=1)
+
+    # fill nas
     item_statistics_closed_df = item_statistics_closed_df.fillna(method="ffill")
 
-    # clean data
-    item_statistics_closed_df = item_statistics_closed_df.drop(['id'], axis=1)
-
-    # convert datettime to time format so we can process days since
+    # convert datetime to time format, so we can process days since
     item_statistics_closed_df['datetime'] = pd.to_datetime(item_statistics_closed_df['datetime'])
 
-    # this is here for tz-naive and tz-aware -- i'm not sure what this means but it fixes it..
+    # this is here for tz-naive and tz-aware -- i'm not sure what this means, but it fixes it..
     current_utc_datetime = datetime.now(timezone.utc)
     item_statistics_closed_df['datetime'] = (current_utc_datetime - item_statistics_closed_df['datetime']).dt.days
 
@@ -160,12 +174,12 @@ def GetItemMarketStatistics(item_url_name, wfm_url=def_wfm_url,
     item_statistics_closed_df = item_statistics_closed_df.rename(columns={'datetime': 'days_ago'})
 
     # cutoff day threshold
-    if ~(item_statistics_closed_df['days_ago'] <= days).any(): # check if there are no entries any less than the days
+    if ~(item_statistics_closed_df['days_ago'] <= days).any():  # check if there are no entries any less than the days
         # of interest
         print(f"\nNo trade data were found for {item_url_name} within {days} days.")
-        item_statistics_closed_df = CreateEmptyHistory()
+        item_statistics_closed_df = CreateEmptyHistory() # if there aren't any then resort to empty df.
     else:
-        item_statistics_closed_df = item_statistics_closed_df.loc[item_statistics_closed_df['days_ago']<= days]
+        item_statistics_closed_df = item_statistics_closed_df.loc[item_statistics_closed_df['days_ago'] <= days]
 
     # filter out top mod rank only.
     if 'mod_rank' in item_statistics_closed_df.columns:
@@ -200,49 +214,35 @@ def GetSummarizedItemMarketStatistics(item_url_name, wfm_url=def_wfm_url,
     return avg_volume, avg_spread, avg_median_price, avg_min_price, avg_typical_price, mod_rank
 
 
-def GetItemMarketStatisticsWithDelay(item_url_name, wfm_url=def_wfm_url, days=accounted_days,
-                                     request_delay=rq_min_delay):
-    # fetch market statistics for all listed items that are available
-    item_statistics_closed_df = GetItemMarketStatistics(item_url_name, wfm_url, days)
-
-    # introduce delay so we don't overwhelm the api
-    time.sleep(request_delay)
-
-    return item_statistics_closed_df
+async def GetSummarizedItemMarketStatisticsAsync(item_url_name, wfm_url=def_wfm_url, days=accounted_days):
+    avg_volume, avg_spread, avg_median_price, avg_min_price, avg_typical_price, mod_rank = await asyncio.to_thread(
+        GetSummarizedItemMarketStatistics, item_url_name, wfm_url, days)
+    return avg_volume, avg_spread, avg_median_price, avg_min_price, avg_typical_price, mod_rank
 
 
-def GetSummarizedItemMarketStatisticsWithDelay(item_url_name, wfm_url=def_wfm_url, days=accounted_days,
-                                               request_delay=rq_min_delay):
-    # fetch market statistics for all listed items that are available
-    result = GetSummarizedItemMarketStatistics(
-        item_url_name,
-        wfm_url, days)
-
-    # introduce delay so we don't overwhelm the api
-    time.sleep(request_delay)
-
-    return result
-
-
-def GetAllSummarizedMarketStatistics(wfm_url=def_wfm_url, days=accounted_days):
-    tqdm.pandas()
-
+async def GetAllSummarizedMarketStatistics(wfm_url=def_wfm_url, days=accounted_days, chunk_size=5, delay = rq_min_delay):
     item_list_df = GetAllItemsList(wfm_url)
 
     numeric_columns = ['avg_volume', 'avg_spread', 'avg_median_price', 'avg_min_price', 'avg_typical_price', 'mod_rank']
 
-    summarized_data_df = pd.DataFrame(columns=['url_name', 'result'])
-    summarized_data_df['url_name'] = item_list_df.index
+    summarized_data_df = pd.DataFrame()
+    summarized_data_df.index = item_list_df.index
 
-    #summarized_data_df = summarized_data_df[:100] #if you want to do this piece wise :)
+    #summarized_data_df = summarized_data_df[0:100]  # if you want to do this piece wise :)
 
-    # for some reason that I don't understand, the apply function returns a tuple no matter what I do, so I will just
-    # take it as it is and just unpack it promptly.
-    summarized_data_df['result'] = summarized_data_df['url_name'].progress_apply(
-        GetSummarizedItemMarketStatisticsWithDelay)
-    summarized_data_df[numeric_columns] = summarized_data_df['result'].apply(pd.Series)
-    summarized_data_df.index = summarized_data_df['url_name']
-    summarized_data_df = summarized_data_df.drop(['result', 'url_name'], axis=1)
+    # the official rate limit is 3 requests per second. chunk_size of 5 and rq delay of 1 works well.
+    for start in tqdm(range(0, len(summarized_data_df), chunk_size)):
+        chunk = summarized_data_df[start:start + chunk_size]
+
+        tasks = [GetSummarizedItemMarketStatisticsAsync(item_name,wfm_url,days) for item_name, _ in chunk.iterrows()]
+        results = await asyncio.gather(*tasks)
+        await asyncio.sleep(delay)
+
+        # Convert results to a DataFrame
+        chunk_results_df = pd.DataFrame(results, columns=numeric_columns, index=chunk.index)
+
+        # Merge chunk_results_df into summarized_data_df using .loc
+        summarized_data_df.loc[chunk_results_df.index, numeric_columns] = chunk_results_df
 
     print(summarized_data_df)
 
